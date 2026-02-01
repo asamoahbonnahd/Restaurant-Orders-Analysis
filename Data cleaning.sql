f76e3fcac25f9b016ec48a06ddcced3298932f7a
Daniel Asamoah <asamoahbonnahd@gmail.com>
2026-01-31 23:09:45 -0500

    Add files via upload

---
+++ Data cleaning.sql
@@ -0,0 +1,29 @@
+SELECT *
+FROM menu_items ;
+
+SELECT *
+FROM order_details ;
+
+
+-- fixing column headers for table menu_items
+
+ALTER TABLE menu_items RENAME COLUMN `ï»¿menu_item_id` TO `Menu_Item_ID` ;
+ALTER TABLE menu_items RENAME COLUMN `item_name` TO `Item_Name` ;
+ALTER TABLE menu_items RENAME COLUMN `category` TO `Category` ;
+ALTER TABLE menu_items RENAME COLUMN `price` TO `Price` ;
+
+
+
+-- fixing column headers for table order_details
+
+ALTER TABLE order_details RENAME COLUMN `ï»¿order_details_id` TO `Order_Details_ID` ;  
+ALTER TABLE order_details RENAME COLUMN `order_id` TO `Order_ID` ;
+ALTER TABLE order_details RENAME COLUMN `order_date` TO `Order_Date` ;
+ALTER TABLE order_details RENAME COLUMN `order_time` TO `Order_Time` ;
+ALTER TABLE order_details RENAME COLUMN `item_id` TO `Item_ID` ;
+
+ALTER TABLE order_details RENAME COLUMN `ï»¿order_details_id` TO `Order_Details_ID` ; 
+
+
+
+
